01d467da029071f6a512d394ad22149e917d9a8b
hoangdoquoc1 <hoangdoquoc1@gmail.com>
2024-12-04 19:15:39 +0700

    Update

---
+++ Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle {
+    private String Type;
+    private String Brand;
+    private String Color;
+    private int Year;
+    public void Run(){
+        System.out.println("The vehicle is running ");
+    }
+}
